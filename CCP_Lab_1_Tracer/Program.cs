230d2ddf407472d88cdf546a817e5f7436f2322f
argaitt <argaitt@gmail.com>
2020-09-13 14:45:36 +0300

    init: initial project

---
+++ CCP_Lab_1_Tracer/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace CCP_Lab_1_Tracer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
