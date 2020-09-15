f9348be7ae11c6e4398ace20d1b4a8b626de59dc
argaitt <argaitt@gmail.com>
2020-09-15 18:14:26 +0300

    add: Tracer class for runtime meter

--- CCP_Lab_1_Tracer/Program.cs
+++ CCP_Lab_1_Tracer/Program.cs
@@ -1,12 +1,26 @@
 ﻿using System;
+using System.Threading;
+using Tracer;
+using System.Diagnostics;
+using System.Collections.Concurrent;
 
 namespace CCP_Lab_1_Tracer
 {
+    
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            
+            Tracer.Tracer tracer1 = new Tracer.Tracer();
+            tracer1.StartTrace();
+            Thread.Sleep(50);
+            tracer1.StopTrace();
+            tracer1.StartTrace();
+            Thread.Sleep(100);
+            tracer1.StopTrace();
+            ConcurrentDictionary<int, TraceResult> arr = new ConcurrentDictionary<int, TraceResult>();
+            arr = tracer1.GetTraceResult();
         }
     }
 }
---
+++ Tracer/Tracer.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Concurrent;
+using System.Collections.Immutable;
+using System.Diagnostics;
+using System.Reflection;
+
+namespace Tracer
+{
+    public struct TraceResult
+    {
+        public long time;
+
+        public string methodName;
+
+        public string className;
+    }
+    interface ITracer 
+    { 
+
+        void StartTrace();
+
+        void StopTrace();
+
+        ConcurrentDictionary<int, TraceResult> GetTraceResult();
+
+    }
+
+    public class Tracer:ITracer
+    {
+        Stopwatch stopwatch;
+        public Tracer() 
+        {
+            stopwatch = new Stopwatch();
+            traceResultsList = new ConcurrentDictionary<int, TraceResult>();
+            key = 0;
+        }
+
+        ConcurrentDictionary<int, TraceResult> traceResultsList;
+        int key;
+        public void StartTrace()
+        {
+            stopwatch.Start();
+            
+        }
+        public void StopTrace()
+        {
+            stopwatch.Stop();
+            TraceResult traceResult;
+            traceResult.time = stopwatch.ElapsedMilliseconds;
+            // получаем имя метода в контекте которого вызван StopTrace()
+            StackTrace stackTrace = new StackTrace();
+            StackFrame frame = stackTrace.GetFrames()[1];
+            MethodBase method = frame.GetMethod();
+            Type type = method.DeclaringType;
+            traceResult.methodName = method.Name;
+            traceResult.className = type.Name;
+            traceResultsList.TryAdd(key, traceResult);
+            key++;
+            
+        }
+        
+        public ConcurrentDictionary<int, TraceResult> GetTraceResult()
+        {
+            return traceResultsList;
+        }
+    }
+}
